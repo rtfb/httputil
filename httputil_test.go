32630a13433f2fcae90dc3f8620eb940aaee095f
Vytautas Šaltenis <vytas@rtfb.lt>
2015-02-14 21:23:34 +0200

    Move code from github.com/rtfb/rtfblog

---
+++ httputil_test.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"net/http"
+	"testing"
+)
+
+func TestExtractReferer(t *testing.T) {
+	req, err := http.NewRequest("GET", "http://example.com", nil)
+	if err != nil {
+		t.Error(err)
+	}
+	refURL := ExtractReferer(req)
+	if refURL != "" {
+		t.Fatalf("Empty string expected, but <%s> found!", refURL)
+	}
+	req.Header.Add("Referer", "foo/bar/baz")
+	refURL = ExtractReferer(req)
+	if refURL != "baz" {
+		t.Fatalf("<baz> expected, but <%s> found!", refURL)
+	}
+}
